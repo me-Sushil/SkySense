1d0f8f38e5ba1e76792fb3285e66ced9fde02325
me-Sushil <girbahadurbk123@gmail.com>
2025-08-12 23:37:33 +0545

    remove comments

--- src/services/data.js
+++ src/services/data.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const lat = 27.7172; // Kathmandu latitude
-const lon = 85.3240; // Kathmandu longitude
+const lat = 27.7172; 
+const lon = 85.3240; 
 const apiKey = "2e2c4ac74b20deb70c35de523b8e2367";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
