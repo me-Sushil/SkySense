35d8359b9a81e8925c1a217871cdedd36a8ee561
me-Sushil <girbahadurbk123@gmail.com>
2025-08-12 23:59:50 +0545

    add

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import getService from "../services/data";
 const Search=()=>{
     const [search, setSearch] = useState("");
+
      useEffect(()=>{
     getService.getAll().then(response=>response);
    setSearch("");
 })
 
 const searchItem =()=>{
-    //experiment not done yet  
  }
   
     return(
