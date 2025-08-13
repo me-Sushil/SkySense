17d710be2fc3b624bccca9604dbb05337fc958be
me-Sushil <girbahadurbk123@gmail.com>
2025-08-14 01:46:34 +0545

    remove unwanted data

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,14 +6,16 @@ const WeatherCard = ({ weatherResult, aqi }) => {
   }
   const sunrise = weatherResult?.sys?.sunrise;
   const sunset = weatherResult?.sys?.sunset;
+  
   const sunriseDate = sunrise
     ? new Date(sunrise * 1000).toLocaleTimeString()
     : "N/A";
+
   const sunsetDate = sunset
     ? new Date(sunset * 1000).toLocaleTimeString()
     : "N/A";
-  //const [aqiVal, setAQIVal] = useState("");
-  let aqiVal = "";
+
+    let aqiVal = "";
   const aQI = aqi?.list?.[0]?.main?.aqi;
   if (aQI === 1) {
     aqiVal ="Good";
