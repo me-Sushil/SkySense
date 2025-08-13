61c79b7ae9af8ae87eb4f10fdd942af6be006030
me-Sushil <girbahadurbk123@gmail.com>
2025-08-13 21:29:42 +0545

    just show all details no logic now

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,21 +1,23 @@
-const WeatherCard=({weatherResult})=>{
-    if(!weatherResult){
-        return <p>Loading weather...</p>;
-    }
-    return(
-        <>
-        <ul>
-            <li>Tempreture : {weatherResult?.main?.temp}</li>
-            <li>Weather condition : {weatherResult?.weather[0]?.description}</li>
-            <li>Humidity : {weatherResult?.main?.humidity}%</li>
-            <li>Wind speed : {weatherResult?.wind?.speed}m/s</li>
-            <li>{weatherResult?.main?.temp}</li>
-            <li>{weatherResult?.main?.temp}</li>
-            <li>{weatherResult?.main?.temp}</li>
-        </ul>
-        
-        
-        </>
-    )
-}
+const WeatherCard = ({ weatherResult }) => {
+  if (!weatherResult) {
+    return <p>Loading weather...</p>;
+  }
+  const sunrise = weatherResult?.sys?.sunrise;
+  const sunset = weatherResult?.sys?.sunset;
+  const sunriseDate = sunrise ? new Date(sunrise * 1000).toLocaleTimeString(): "N/A";
+  const sunsetDate = sunset ? new Date(sunset * 1000).toLocaleTimeString(): "N/A";
+
+  return (
+    <>
+      <ul>
+        <li>Tempreture : {weatherResult?.main?.temp}</li>
+        <li>Weather condition : {weatherResult?.weather?.[0]?.description}</li>
+        <li>Humidity : {weatherResult?.main?.humidity}%</li>
+        <li>Wind speed : {weatherResult?.wind?.speed}m/s</li>
+        <li>Sunrise : {sunriseDate}</li>
+        <li>Sunset : {sunsetDate}</li>
+      </ul>
+    </>
+  );
+};
 export default WeatherCard;
