1cdfb5b688eefc17ffc72bf804c73eade1a9761c
me-Sushil <girbahadurbk123@gmail.com>
2025-08-13 20:57:44 +0545

    show wind speed

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -7,8 +7,8 @@ const WeatherCard=({weatherResult})=>{
         <ul>
             <li>Tempreture : {weatherResult?.main?.temp}</li>
             <li>Weather condition : {weatherResult?.weather[0]?.description}</li>
-            <li>Humidity : {weatherResult?.main?.humidity}</li>
-            <li>{weatherResult?.main?.temp}</li>
+            <li>Humidity : {weatherResult?.main?.humidity}%</li>
+            <li>Wind speed : {weatherResult?.wind?.speed}m/s</li>
             <li>{weatherResult?.main?.temp}</li>
             <li>{weatherResult?.main?.temp}</li>
             <li>{weatherResult?.main?.temp}</li>
