47b77ccfa6d25e0a3a0b5acb68cca5db8a145ef6
me-Sushil <girbahadurbk123@gmail.com>
2025-08-15 00:01:50 +0545

    all css code manage

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,3 @@
-
 // import searchlogo from "../assets/Search.png";
 import { useEffect, useState } from "react";
 import WeatherCard from "./WeatherCard";
@@ -8,6 +7,8 @@ const Search = () => {
   const [weatherResult, setWeatherResult] = useState([]);
   const [city, setCity] = useState([]);
   const [aqiData, setAQIData] = useState([]);
+  const [lat, setLat] = useState(27.7083)
+  const [lon, setLon] = useState(85.3206)
   const apiKey = "2e2c4ac74b20deb70c35de523b8e2367";
  
   useEffect(() => {
@@ -22,7 +23,8 @@ const Search = () => {
       axios
         .get(geourl)
         .then((response) => {
-             
+          setLat(response.data.lat);
+          setLon(response.data.lon);
           setCity(response.data);
           console.log(response.data, "response data");
         })
@@ -31,8 +33,41 @@ const Search = () => {
     return () => clearTimeout(delaySearchQry);
   }, [searchQry]);
 
-  const handleCityClick = (lat, lon) => {
-    
+  useEffect(()=>{
+axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+      )
+      .then((response) => {
+        setWeatherResult(response.data);
+        console.log(response.data);
+      });
+
+    // // if (city.country === "NP") {
+    //   axios
+    //     .get(
+    //        `https://api.waqi.info/feed/${searchQry}/?token=c6b3f4fa907564603e0e27b8bee35f9051fdc35f`// waqi AQI api provider but there is no data of nepal
+    //       //`https://api.waqi.info/feed/@H9468/?token=c6b3f4fa907564603e0e27b8bee35f9051fdc35f`// American ambessy AQI in nepal ambessy area only
+    //     )
+    //     .then((response) => {
+    //       setAQIData(response.data);
+    //       console.log("AQI data api without nepali data",response.data);
+    //     });
+    // // } else {
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      )
+      .then((response) => {
+        setAQIData(response.data);
+        console.log("world data AQI api including nepal", response.data);
+      });
+},[]);
+
+
+  const handleCityClick = (city, lat, lon) => {
+    localStorage.setItem("city", JSON.stringify(city));
     setCity([]);
     axios
       .get(
@@ -85,7 +120,7 @@ const Search = () => {
             {city.map((city, index) => (
               <li 
                 key={index}
-                onClick={() => handleCityClick(city.lat, city.lon)}
+                onClick={() => handleCityClick(city.name, city.lat, city.lon)}
               >
                 {city.name}, {city.country}
               </li>
@@ -97,7 +132,7 @@ const Search = () => {
       </div>
       </div>
       
-      <div className="weatherCard">
+       <div > {/*className="weatherCard" */}
       <WeatherCard aqi={aqiData} weatherResult={weatherResult} />
     </div>
     </>
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,17 +1,18 @@
 // import { useState } from "react";
 import "../App.css";
 
-
 import { useState } from "react";
 
 const WeatherCard = ({ weatherResult, aqi }) => {
   // if (!weatherResult) {
   //   return <p>Loading weather</p>;
   // }
+  console.log(weatherResult.name, "test for city");
+  const recentSearchCity = JSON.parse(localStorage.getItem("city"));
   const sunrise = weatherResult?.sys?.sunrise;
   const sunset = weatherResult?.sys?.sunset;
-  const windSpeedKM = ((weatherResult?.wind?.speed)*3.6).toFixed(2);
-  
+  const windSpeedKM = (weatherResult?.wind?.speed * 3.6).toFixed(2);
+
   const sunriseDate = sunrise
     ? new Date(sunrise * 1000).toLocaleTimeString()
     : "N/A";
@@ -20,51 +21,67 @@ const WeatherCard = ({ weatherResult, aqi }) => {
     ? new Date(sunset * 1000).toLocaleTimeString()
     : "N/A";
 
-    let aqiVal = "";
+  let aqiVal = "";
   const aQI = aqi?.list?.[0]?.main?.aqi;
   if (aQI === 1) {
-    aqiVal ="Good";
+    aqiVal = "Good";
   } else if (aQI === 2) {
-    aqiVal ="Fair";
+    aqiVal = "Fair";
   } else if (aQI === 3) {
-    aqiVal ="Moderate";
+    aqiVal = "Moderate";
   } else if (aQI === 4) {
-    aqiVal ="Unhealthy for Sensitive Groups";
-  } else if (aQI === 5){
-    aqiVal ="Unhealthy";
-  }else{
+    aqiVal = "Unhealthy for Sensitive Groups";
+  } else if (aQI === 5) {
+    aqiVal = "Unhealthy";
+  } else {
     aqiVal = "N/A";
   }
 
   const [isCelsius, setIsCelsius] = useState(true);
-    const cel = weatherResult?.main?.temp;
-  
-  const changeTempreture=()=>{
-    setIsCelsius((prev)=>!prev);
-  }
-  const displayTempData = isCelsius ? `${cel}°C`: `${(cel*(9/5))+32}°F`;
+  const cel = Math.floor(weatherResult?.main?.temp);
+
+  const changeTempreture = () => {
+    setIsCelsius((prev) => !prev);
+  };
+  const displayTempData = isCelsius ? `${cel}°C` : `${cel * (9 / 5) + 32}°F`;
 
   return (
     <>
-      <ul>
-        <li>Tempreture : {displayTempData} <button className="BTN" onClick={changeTempreture}>toggle</button></li>
-        <li>Weather condition : {weatherResult?.weather?.[0]?.description}</li>
-        <li>Humidity : {weatherResult?.main?.humidity}%</li>
-        <li>Wind speed : {windSpeedKM}km/h</li>
-        <li>Sunrise : {sunriseDate}</li>
-        <li>Sunset : {sunsetDate}</li>
-        <li>AQI : {aqiVal}</li>
-      </ul>
-      <div>
-        <ul>
-        <li>Tempreture : {displayTempData} <button className="BTN" onClick={changeTempreture}>toggle</button></li>
-        <li>Weather condition : {weatherResult?.weather?.[0]?.description}</li>
-        <li>Humidity : {weatherResult?.main?.humidity}%</li>
-        <li>Wind speed : {windSpeedKM}km/h</li>
-        <li>Sunrise : {sunriseDate}</li>
-        <li>Sunset : {sunsetDate}</li>
-        <li>AQI : {aqiVal}</li>
-      </ul>
+      <div className="weatherCard">
+        <div className="weatherCard-left">
+          <div className="mainWeatherInfo">
+            <p>
+              Tempreture : {displayTempData}
+              <button className="BTN" onClick={changeTempreture}>
+                toggle
+              </button>
+            </p>
+            <p>
+              Weather condition : {weatherResult?.weather?.[0]?.description}
+            </p>
+            <p> Humidity : {weatherResult?.main?.humidity}%</p>
+            <p> Wind speed : {windSpeedKM}km/h</p>
+            <p> Sunrise : {sunriseDate}</p>
+            <p> Sunset : {sunsetDate}</p>
+            <p> AQI : {aqiVal}</p>
+          </div>
+          <div className="bigData">
+            <p style={{
+                fontSize: "66px",
+                margin: "10px 0",}}>{displayTempData}</p>{weatherResult.name}</div>
+        </div>
+        <div className="recentSearchCity">
+          <p>
+            Tempreture : {displayTempData}
+            <button className="BTN" onClick={changeTempreture}>
+              toggle
+            </button>
+          </p>
+          <p> Weather condition : {weatherResult?.weather?.[0]?.description}</p>
+          <p> Humidity : {weatherResult?.main?.humidity}%</p>
+          <p> Wind speed : {windSpeedKM}km/h</p>
+          <p> Sunrise : {sunriseDate}</p>
+        </div>
       </div>
     </>
   );
