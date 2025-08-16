a81f1b67575c686491ec788adaf937818c378eef
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-17 00:30:37 +0545

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import logo from "../assets/logo.png";
 import dark from "../assets/MoonDarkMood.png";
 import light from "../assets/SunLightMood.png";
 import "@fontsource/jacquard-24"; // Defaults to weight 400
-// import "../style/header.css";
 const Header = () => {
   return (
     <>
