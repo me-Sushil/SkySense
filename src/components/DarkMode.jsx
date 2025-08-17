1b7836ba40c6a24b84aba0c8d8edd20e27d8281a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-17 13:14:34 +0545

    impliment toggle

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,17 +1,50 @@
 import dark from "../assets/MoonDarkMood.png";
 import light from "../assets/SunLightMood.png";
 import "../App.css";
-// import useState from "react";
+import { useState, useEffect } from "react";
 const DarkMode = () => {
-    // const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    
+  useEffect(() => {
+    const savedMode = JSON.parse(localStorage.getItem("theme"));
+    if (savedMode === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, []);
+
+  const handleButtonClick = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+
+    if (newTheme) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", JSON.stringify("dark"));
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", JSON.stringify("light"));
+    }
+  };
+
   return (
     <>
-      <div data-theme="dark" >
-        <button className="mood">
-          <img src={dark} className="darkmood" />
-          <img src={light} className="lightmood"/>
+      <div>
+        <button className="mood" onClick={handleButtonClick}>
+          <img
+            src={dark}
+            className="darkmood"
+            alt="Dark mode"
+            style={{ opacity: isDarkMode ? 1 : 0.5 }}
+          />
+          <img
+            src={light}
+            className="lightmood"
+            alt="Light mode"
+            style={{ opacity: isDarkMode ? 0.5 : 1 }}
+          />
         </button>
       </div>
     </>
