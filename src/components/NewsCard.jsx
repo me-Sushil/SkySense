d02fc41f39056064bb9c5ba9be568386e2edf78a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-17 16:14:50 +0545

    send city name to newscard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,8 +1,11 @@
-const NewsCard=({searchQry})=>{
-    return(
-        <>
-        {searchQry}
-        </>
-    )
-}
+const NewsCard = ({ city }) => {
+    console.log(city,"city for news");
+  return (
+    <>
+    <div>
+        Recent Top 5 News of {city}
+    </div>
+    </>
+  );
+};
 export default NewsCard;
